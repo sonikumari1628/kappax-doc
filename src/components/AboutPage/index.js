620b3e8fbfbd1cb9275eae0541c597ecbd714641
sonikumari1628 <sonitiwari1625@gmail.com>
2023-12-24 07:18:23 +0530

    first commit

---
+++ src/components/AboutPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss'; 
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1 className='about'>About KappaX</h1>
+        <h3>Hello!!!!!</h3>
+    </div>
+    
+  )
+}
+
+export default AboutPage;
